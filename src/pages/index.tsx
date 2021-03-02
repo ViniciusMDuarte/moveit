32ef5da92ada89f0d9533d9473737fed7675d8ca
ViniciusMDuarte <viniduarte222@gmail.com>
2021-03-01 22:58:07 -0300

    finalized countdown  and initialized challengbox

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,12 @@ import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+import { ChallengeBox } from "../components/ChallengeBox";
+
 import styles from '../styles/pages/Home.module.css'
 
 import Head from 'next/head'
+import React from "react";
 
 export default function Home() {
   return (
@@ -23,7 +26,8 @@ export default function Home() {
         <Countdown />
       </div>
       <div>
-        
+
+        <ChallengeBox />
 
       </div>
       
